0af3f2862ee961e3dcd9c300a69c052244ab7af7
EdenQwQ <lsahlm1eden@gmail.com>
2025-03-04 14:28:31 +0800

    nix: remove unused cachix

--- nix/substituters.nix
+++ nix/substituters.nix
@@ -8,7 +8,6 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
-      "https://nixpkgs-wayland.cachix.org"
       "https://niri.cachix.org"
     ];
 
@@ -16,7 +15,6 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     ];
   };
