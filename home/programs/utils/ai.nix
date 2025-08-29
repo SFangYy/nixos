d37eaeeac139cbb42693ec6646fddf9a9684a6d7
EdenQwQ <lsahlm1eden@gmail.com>
2025-08-29 13:12:59 +0800

    add api keys

--- home/programs/utils/ai.nix
+++ home/programs/utils/ai.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.gemini-cli = {
     enable = true;
@@ -8,6 +8,14 @@
       preferredEditor = "nvim";
       autoAccept = false;
     };
+  };
+  programs.claude-code = {
+    enable = true;
+  };
+  home.sessionVariables = {
+    GEMINI_API_KEY = "$(cat ${config.age.secrets.gemini_token.path})";
+    ANTHROPIC_AUTH_TOKEN = "$(cat ${config.age.secrets.anyrouter_token.path})";
+    ANTHROPIC_BASE_URL = "https://anyrouter.top";
   };
   home.packages = with pkgs; [ qwen-code ];
 }
