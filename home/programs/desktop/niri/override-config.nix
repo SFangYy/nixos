af6bed391c7e006283b081858c38c54d7754484f
EdenQwQ <lsahlm1eden@gmail.com>
2025-11-24 18:21:53 +0800

    update niri; recent window config

--- home/programs/desktop/niri/override-config.nix
+++ home/programs/desktop/niri/override-config.nix
@@ -3,7 +3,36 @@ with config.lib.stylix.colors.withHashtag;
 let
   extraConfig =
     # kdl
-    '''';
+    ''
+      recent-windows {
+          // off
+          open-delay-ms 150
+
+          highlight {
+              active-color "${base0E}aa"
+              urgent-color "${base08}aa"
+              padding 30
+              corner-radius 20
+          }
+
+          previews {
+              max-height 480
+              max-scale 0.5
+          }
+
+          binds {
+              Alt+Tab         { next-window; }
+              Alt+Shift+Tab   { previous-window; }
+              Alt+grave       { next-window     filter="app-id"; }
+              Alt+Shift+grave { previous-window filter="app-id"; }
+
+              Mod+Tab         { next-window; }
+              Mod+Shift+Tab   { previous-window; }
+              Mod+grave       { next-window     filter="app-id"; }
+              Mod+Shift+grave { previous-window filter="app-id"; }
+          }
+      }
+    '';
   finalNiriConfig =
     builtins.replaceStrings
       [
