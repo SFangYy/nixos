c7fdb01c4bc830fd80a845ecf8ef5c3a174974fc
EdenQwQ <lsahlm1eden@gmail.com>
2025-12-05 12:01:26 +0800

    update office

--- home/programs/documents/default.nix
+++ home/programs/documents/default.nix
@@ -4,7 +4,7 @@
     ./zathura.nix
   ];
   home.packages = with pkgs; [
-    inputs.nixpkgs-stable.legacyPackages.${pkgs.stdenv.hostPlatform.system}.libreoffice
-    inputs.nixpkgs-stable.legacyPackages.${pkgs.stdenv.hostPlatform.system}.onlyoffice-desktopeditors
+    libreoffice
+    onlyoffice-desktopeditors
   ];
 }
